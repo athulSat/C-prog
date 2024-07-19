fb939086633daa2eac98d28d10eec1f33845e628
Athul Satheesh <138150043+athulSat@users.noreply.github.com>
2024-07-19 20:57:20 +0530

    Add files via upload

---
+++ L4_23.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int sum=0,flag=0;
+    for(int i=10; i<100; i++)
+	 {
+		  flag = 0;
+		  for(int j=2; j <i; j++)
+		  {
+			   if(i%j==0)
+			   {
+				    flag=1;
+				    break;
+			   }
+		  }
+		  if(flag==0 && i>=2)
+		  {
+		   	sum+=i;
+		  }
+	 }
+    printf("the total number of single digit Prime numbers %d",sum);
+}
